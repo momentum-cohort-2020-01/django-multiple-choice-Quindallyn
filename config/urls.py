e89f5aa0186565ee9037c178d7e9b4043259c287
Quindallyn <qrk5002@gmail.com>
2020-03-10 13:37:34 -0400

    correct structure of files

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.conf import settings
 from django.urls import include, path
 
+from core import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
@@ -25,6 +27,7 @@ if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls)),
+        path('', views.index, name='index')
 
         # For django versions before 2.0:
         # url(r'^__debug__/', include(debug_toolbar.urls)),
