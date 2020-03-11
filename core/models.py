c3baf9f4b8af9a1be6ccadbd6377260f14480765
Quindallyn <qrk5002@gmail.com>
2020-03-11 15:17:56 -0400

    user displaying deck

--- core/models.py
+++ core/models.py
@@ -1,12 +1,14 @@
 from django.db import models
+from users.models import User
 
 class Flashcard(models.Model):
     side1 = models.TextField(max_length=None)
     side2 = models.TextField(max_length=None)
-    # category = models.ForeignKey('Category', on_delete=models.DO_NOTHING, blank=True, null=True)
+    deck = models.ForeignKey('Deck', on_delete=models.CASCADE)
 
 class Deck(models.Model):
     title = models.CharField(max_length=100, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Results(models.Model):
     pass
--- core/views.py
+++ core/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
+from .models import Deck, Flashcard
+
 def deck_list (request):
-    return render(request, 'core/decklist.html') 
+    decks=Deck.objects.all()
+    context = {"decks":decks}
+    return render(request, 'core/decklist.html', context=context) 
 
 # def flascard_runthrough (request):
 #     return render(request, 'core/flashcardrunthrough.html')
